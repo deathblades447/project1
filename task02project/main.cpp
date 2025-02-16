b9759c2726d7b78bd527747e5dbe2aeccd2c2e34
rasslabsya <artyomlordik@gmail.com>
2025-02-16 18:48:07 +0300

    добавление в новый проект функции swap и тестирование передачи данных по ссылке

---
+++ task02project/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void swap(int a, int b);
+
+
+int main() {
+	int y = 10, x = 7;
+
+	cout << "before: x = " << x << ",y = "<< y << endl;
+	swap(x,y);
+	cout << "after: x = " << x << ",y = " << y << endl;
+
+
+	return 0;
+
+}
